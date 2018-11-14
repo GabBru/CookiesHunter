f6fb41ab2e2761b8325e152e88a1f70583f52fb7
Nairy86 <45037891+Nairy86@users.noreply.github.com>
2018-11-14 18:06:04 +0100

    Create Npc.java

---
+++ Npc.java
@@ -0,0 +1,12 @@
+/**
+ * Npc is a class of non-player character, not agressive, with a different constructor.
+ * @Amandine POULLOT & Gabriel BRUNET
+ * @version 1.0 14/11/2018
+ */
+
+public class Npc extends Character
+{
+    public Npc(String name, Room currentRoom)
+    {
+    }
+}
