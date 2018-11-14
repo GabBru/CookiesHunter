18d9b6b19dd8649b36b9a2e1771b6a2762848bed
kfelesmi <45011529+kfelesmi@users.noreply.github.com>
2018-11-14 17:52:12 +0100

    Add files via upload

---
+++ Room.java
@@ -0,0 +1,72 @@
+import java.util.HashMap;
+import java.util.*;
+
+/**
+ * Class Room - a room in an adventure game.
+ *
+ * This class is part of the "World of Zuul" application. 
+ * "World of Zuul" is a very simple, text based adventure game.  
+ *
+ * A "Room" represents one location in the scenery of the game.  It is 
+ * connected to other rooms via exits.  The exits are labelled north, 
+ * east, south, west,up and down.  For each direction, the room stores a reference
+ * to the neighboring room, or null if there is no exit in that direction.
+ * 
+ * @author  Diama Fall & Kilian FELESMINO
+ * @version 2018.11
+ *
+ */
+public class Room   
+{
+    private String description;
+    private HashMap<String, Room> exits;
+    private ArrayList<Item> roomItem;
+
+    /**
+     * Create a room described "description". Initially, it has
+     * no exits. "description" is something like "a kitchen" or
+     * "an open court yard".
+     * @param description The room's description.
+     */
+    public Room(String description) 
+    {
+        if (description.length()<=1) throw new IllegalArgumentException("Description courte");
+        this.description = description;
+        exits = new HashMap<>();
+        roomItem = new ArrayList<>();
+    }
+
+
+    /**
+     * Method getExit: returns the room that we reach in the given direction
+     * If there is no room in that direction, returns null
+     *
+     * @param direction The exit's direction
+     * @return The roomin the given direction
+     * 
+     */
+     
+    public Room getExit(String direction){
+            return exits.get(direction);
+    }
+    
+    /**
+     * Method setExit: defines an exit from this room
+     * Replaces the setExits method
+     *
+     * @param direction the direction of the exit
+     * @param neighbor the room in the given direction
+       */
+    public void setExit(String direction, Room neighbor){
+            exits.put(direction, neighbor);
+    }
+       
+    /**
+     * @return The description of the room.
+     */
+    public String getDescription()
+    {
+        return description;
+    }
+
+}
---
+++ RoomTest.java
@@ -0,0 +1,88 @@
+
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Classe-test RoomTest.
+ *
+ * @author  Diama FALL
+ * @version 2018/11
+ *
+ * Les classes-test sont documentées ici :
+ * http://junit.sourceforge.net/javadoc/junit/framework/TestCase.html
+ * et sont basées sur le document Š 2002 Robert A. Ballance intitulé
+ * "JUnit: Unit Testing Framework".
+ *
+ * Les objets Test (et TestSuite) sont associés aux classes à tester
+ * par la simple relation yyyTest (e.g. qu'un Test de la classe Name.java
+ * se nommera NameTest.java); les deux se retrouvent dans le męme paquetage.
+ * Les "engagements" (anglais : "fixture") forment un ensemble de conditions
+ * qui sont vraies pour chaque méthode Test à exécuter.  Il peut y avoir
+ * plus d'une méthode Test dans une classe Test; leur ensemble forme un
+ * objet TestSuite.
+ * BlueJ découvrira automatiquement (par introspection) les méthodes
+ * Test de votre classe Test et générera la TestSuite conséquente.
+ * Chaque appel d'une méthode Test sera précédé d'un appel de setUp(),
+ * qui réalise les engagements, et suivi d'un appel à tearDown(), qui les
+ * détruit.
+ */
+public class RoomTest
+{
+    private Room roomDescription ;
+    
+    
+    /**
+     * Constructor of the RoomTest class alllow to initializ correctly the
+     */
+    public RoomTest()
+    {
+        
+    }
+
+    /**
+     * Met en place les engagements.
+     *
+     * Méthode appelée avant chaque appel de méthode de test.
+     */
+    @Before
+    public void setUp() // throws java.lang.Exception
+    {
+       roomDescription = new Room ("azertyu");
+    }
+    /**
+     * 
+     */
+    @Test
+    public void testSetExit() {
+        Room thing = new Room("sxcvghuk");
+        roomDescription.setExit("north",thing);
+        assertEquals(thing, roomDescription.getExit("north"));
+    }
+
+    /**
+     * Supprime les engagements
+     *
+     * Méthode appelée après chaque appel de méthode de test.
+     */
+    @After
+    public void tearDown() // throws java.lang.Exception
+    {
+        //Libérez ici les ressources engagées par setUp()
+    }
+    /**
+     * 
+     */
+    @Test
+    public void testDescription(){
+        boolean result = true;
+        try{
+            Room test = new Room("");
+        } catch (IllegalArgumentException e) {
+            result=false;
+        }
+        assertEquals(false,result);
+    }
+}
