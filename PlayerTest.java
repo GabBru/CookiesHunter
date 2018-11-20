2f7698393588045aab1d136de62065553b84deff
Gabriel Brunet <33067666+GabBru@users.noreply.github.com>
2018-11-20 18:21:21 +0100

    Create PlayerTest.java

---
+++ PlayerTest.java
@@ -0,0 +1,60 @@
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import java.util.*;
+
+/**
+ * The test class PlayerTest.
+ *
+ * @author  Brunet Gabriel & Amandine Poullot
+ * @version v0.1 2018nov
+ */
+public class PlayerTest
+{
+    private Player testPlayerCharacter;
+    private ArrayList<Item> testInvent;
+    private Item Cookie;
+    private Room Grenier; 
+    private String playerName;
+    
+    /**
+     * Default constructor for test class PlayerTest
+     */
+    public PlayerTest()
+    {
+    }
+
+    /**
+     * Sets up the test fixture.
+     *
+     * Called before every test case method.
+     */
+    @Before
+    public void setUp()
+    {
+        testPlayerCharacter= new Player(playerName,Grenier);
+        
+    }
+
+    /**
+     * Tears down the test fixture.
+     *
+     * Called after every test case method.
+     */
+    @After
+    public void tearDown()
+    {
+    }
+    
+    /**
+     * Method testConstructorCharacter : method that checked if setting
+     * level is correctly done.
+     */
+    @Test
+    public void testSetLevel()
+    {
+        testPlayerCharacter.setLevel(23);
+        assertEquals(23,testPlayerCharacter.getLevel());
+    }
+}
