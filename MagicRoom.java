10c93ff9f243e5b8da567b0fa8283bcacccce228
facina37 <45038209+facina37@users.noreply.github.com>
2018-11-21 15:28:47 +0100

    Update MagicRoom.java

--- MagicRoom.java
+++ MagicRoom.java
@@ -3,34 +3,57 @@ import java.util.*;
  * It is a class who inherites from Room. This room needs a password to be opened.
  *
  * @author GIL Thibaud
- * @version 11/14/2018
+ * @version (2)
  */
  
 public class MagicRoom extends Room
 {
     //Variables d'instance
-    private boolean isLocked; //Boolean represe
-    private String magicPass, passWord;
+    private boolean isLocked; //The isLocked boolean define if the room is closed or open.
+    private String passWord; //The passWord String 
     //Constructor for object of class MagicRoom
 
     public MagicRoom (String newDescription, String passWord)
     {
         super(newDescription);
-        this.magicPass = passWord;
+        this.passWord = passWord;
     }
 
-    public void setPassword (String newPassWord)
+    /** 
+    * Modify the passWord of rooms
+    */
+   public void setPassword (String newPassWord)
     {
         this.passWord = newPassWord;
     }
-
-    // Return to password
-    public void getPassword()
+    
+    /** 
+    * Return the password of room
+    */
+   public String getPassword()
     {
+        return passWord;
     }
 
-    public void LockRoom()
+    /** 
+    * Modify the boolean isLocked
+    */
+   public void setIsLocked()
     {
+        if (isLocked == true)
+            isLocked = false;
+        else
+            isLocked = true;
     }
 
+   /** 
+    * Return the boolean isLocked
+    */
+   public boolean getIsLocked()
+    {
+        if (isLocked = true)
+            return true;
+        else
+            return false;
+    }
 }
