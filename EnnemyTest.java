fe7a834188e666fd9d8cdbaef879571b90607494
Gabriel Brunet <33067666+GabBru@users.noreply.github.com>
2018-11-21 15:25:22 +0100

    Create EnnemyTest.java

---
+++ EnnemyTest.java
@@ -0,0 +1,56 @@
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * The test class EnnemyTest.
+ *
+ * @author  Gabriel Brunet & Amandine Poullot
+ * @version v0.1 2018nov
+ */
+public class EnnemyTest
+{
+    protected int level;
+    private boolean isDead;
+    protected Ennemy testEnnemyCharacter;
+    private Room Grenier;
+    
+    /**
+     * Default constructor for test class EnnemyTest
+     */
+    public EnnemyTest()
+    {
+    }
+
+    /**
+     * Sets up the test fixture.
+     *
+     * Called before every test case method.
+     */
+    @Before
+    public void setUp()
+    {
+        testEnnemyCharacter = new Ennemy("Bebe",Grenier,10);
+    }
+
+    /**
+     * Tears down the test fixture.
+     *
+     * Called after every test case method.
+     */
+    @After
+    public void tearDown()
+    {
+    }
+    
+    /**
+     * Check the availability of the set methods for the boolean isDead.
+     */
+    @Test
+    public void testSetIsDead()
+    {
+        testEnnemyCharacter.setIsDead(true);
+        assertEquals(true,testEnnemyCharacter.getIsDead());
+    }
+}
