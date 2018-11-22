24c811a6b02efc4be654229cb61eda52bde2b5e3
Gabriel Brunet <33067666+GabBru@users.noreply.github.com>
2018-11-22 12:18:09 +0100

    Update LockRoomTest.java

--- LockRoomTest.java
+++ LockRoomTest.java
@@ -2,101 +2,81 @@ import static org.junit.Assert.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-
 /**
- * Classe-test LockRoomTest.
+ * Classe-test LockRoomTest - for LockRoom class.
  *
  * @author  Antoine PIERSON
- * @version 1.0
+ * @version v0.1 2018nov
  *
- * Les classes-test sont documentÃ©es ici :
+ * Les classes-test sont documentees ici :
  * http://junit.sourceforge.net/javadoc/junit/framework/TestCase.html
- * et sont basÃ©es sur le document Å  2002 Robert A. Ballance intitulÃ©
+ * et sont basees sur le document Å  2002 Robert A. Ballance intitule
  * "JUnit: Unit Testing Framework".
  *
- * Les objets Test (et TestSuite) sont associÃ©s aux classes Ã  tester
- * par la simple relation yyyTest (e.g. qu'un Test de la classe Name.java
- * se nommera NameTest.java); les deux se retrouvent dans le mÄ™me paquetage.
- * Les "engagements" (anglais : "fixture") forment un ensemble de conditions
- * qui sont vraies pour chaque mÃ©thode Test Ã  exÃ©cuter.  Il peut y avoir
- * plus d'une mÃ©thode Test dans une classe Test; leur ensemble forme un
- * objet TestSuite.
- * BlueJ dÃ©couvrira automatiquement (par introspection) les mÃ©thodes
- * Test de votre classe Test et gÃ©nÃ©rera la TestSuite consÃ©quente.
- * Chaque appel d'une mÃ©thode Test sera prÃ©cÃ©dÃ© d'un appel de setUp(),
- * qui rÃ©alise les engagements, et suivi d'un appel Ã  tearDown(), qui les
- * dÃ©truit.
  */
 public class LockRoomTest
 {
-    private LockRoom ok;
+    private LockRoom okLockRoom;
     private String myDescription;
-    
     /**
-     * Constructeur de la classe-test LockRoomTest
+     * Classe-test LockRoomTest constructor.
      */
     public LockRoomTest()
     {
     }
 
     /**
-     * Met en place les engagements.
+     * Sets up the test fixture.
      *
-     * Method called before each test methods calls.
+     * Called before every test case method.
+     * 
+     * @param goodRoom Object used to do tests in general. This object respesct each constructor restrictions.
+     * @param myDescription String used for the parameter "description" in constructor.
      */
     @Before
-    public void setUp() // throws java.lang.Exception
+    public void setUp()
     {
-        // Initialisez ici vos engagements
         myDescription = "You are in the kitchen";
-        
-        //creation of a good locked room
         LockRoom goodRoom = new LockRoom(myDescription);
     }
-
+    
     /**
-     * Method testConstructorLockRoom : 
-     * Test creating if the boolean isLocked is FALSE
+     * Method testConstructorLockRoom : Check the good operation of the constructor.
      */
     @Test
     public void testConstructorLockRoom()
     {
         boolean result = true;
         try{
-            ok = new LockRoom(myDescription);
+            okLockRoom = new LockRoom(myDescription);
         } catch (IllegalArgumentException e){
             result = false;
         }
         assertEquals(true, result);
-        // Check if my test is ok
-        assertEquals(myDescription, ok.getDescription());
-        // Check if the boolean isLocked is false
-        assertEquals(false, ok.getLocked());
+        assertEquals(myDescription, okLockRoom.getDescription());
+        assertEquals(false, okLockRoom.getLocked());
     }
     
     /**
-     * Method testSetIsLocked:
-     * test if the setter change the boolean
+     * Method testSetIsLocked: Test if the setter change the boolean correctly.
      */
     @Test
     public void testSetIsLocked()
     {
-        // by default isLocked is False, so setLocked change isLocked in true
-        ok.setLocked();
-        assertEquals(true, ok.getLocked());
-        // isLocked is true, so setLocked change isLocked in false
-        ok.setLocked();
-        assertEquals(false, ok.getLocked());
+        okLockRoom.setLocked();
+        assertEquals(true, okLockRoom.getLocked());
+        okLockRoom.setLocked();
+        assertEquals(false, okLockRoom.getLocked());
     }
     
     /**
-     * Supprime les engagements
+     * Tears down the test fixture.
      *
-     * MÃ©thode appelÃ©e aprÃ¨s chaque appel de mÃ©thode de test.
+     * Called after every test case method.
      */
     @After
-    public void tearDown() // throws java.lang.Exception
+    public void tearDown()
     {
-        //LibÃ©rez ici les ressources engagÃ©es par setUp()
+        //Liberez ici les ressources engagees par setUp()
     }
 }
