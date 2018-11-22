525d94100eedeb3d683a1aea1a6721f69d89bbaf
Gabriel Brunet <33067666+GabBru@users.noreply.github.com>
2018-11-22 12:48:52 +0100

    Update MagicRoom.java

--- MagicRoom.java
+++ MagicRoom.java
@@ -1,56 +1,63 @@
 import java.util.*;
 /**
- * It is a class who inherites from Room. This room needs a password to be opened.
+ * Subclass Ennemies inherites the superclass Character. 
+ * Like the LockRoom, this one needs an entity (here a String passWord) to be unlocked.
  *
  * @author GIL Thibaud & Kilian Felesmino
  * @version v0.2 2018nov
  */
- 
 public class MagicRoom extends Room
 {
-    //Variables d'instance
-    private boolean isLocked; //The isLocked boolean define if the room is closed or open.
-    private String passWord; //The passWord String 
-    //Constructor for object of class MagicRoom
-
-    public MagicRoom (String newDescription, String passWord)
-    {
-        super(newDescription);
-        this.passWord = passWord;
-    }
-
-    /** 
-    * Modify the passWord of rooms
+    private boolean isLocked;
+    private String passWord;
+   /**
+    * Constructor 
+    * Create a new MagicRoom.
+    * As one of the subclass of the LockRoom superclass, each attributes of this class has the same restrictions.
+    * The password not have restrictions about number of characters but must not contain any spaces.
+    * If there is a space in the password, the object cannot be created and an exception is returned.
+    * 
+    * @param newDescription See details in the superclass.
+    * @param passWord Must be compare to a given String to be changed as an unlockedRoom. If the two spring are not the same, the room stay blocked and the Player cannot go inside.
+    * @exception IllegalArgumentException Returned in case of passWord troubles (spaces).
+    */
+   public MagicRoom (String newDescription, String passWord)
+   {
+       super(newDescription);
+       if ((passWord.length() - passWord.replace(" ","").length())>0) throw new IllegalArgumentException("Incorrect password");
+       this.passWord = passWord;
+   }
+   
+   /** 
+    * Able to change the value of the String passWord. Should not be used once the object created whithout exception. 
     */
    public void setPassword (String newPassWord)
-    {
-        this.passWord = newPassWord;
-    }
-    
-    /** 
-    * Return the password of room
+   {
+       this.passWord = newPassWord;
+   }
+   
+   /** 
+    * @return Return the string corresponding to the passWord.
     */
    public String getPassword()
-    {
-        return passWord;
-    }
-
-    /** 
-    * Modify the boolean isLocked
-    */
-   public void setIsLocked()
-    {
-        if (isLocked == true)
-            isLocked = false;
-        else
-            isLocked = true;
-    }
+   {
+       return passWord;
+   }
 
    /** 
-    * Return the boolean isLocked
+    * Method changing automatically the value of the boolean isLocked. Switch the state "true" or false" to its inverse.
+    */
+   public void setIsLocked()
+   {
+       if (isLocked == true) {isLocked = false;}
+       else {isLocked = true;}
+   }
+
+   /** 
+    * @return Return the boolean isLocked.
     */
    public boolean getIsLocked()
-    {
-        return isLocked;
-    }
+   {
+       return isLocked;
+   }
 }
