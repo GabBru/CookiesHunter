b428177ff1ef9b848a0b5c060636dc9fbb4bcd52
Gabriel Brunet <33067666+GabBru@users.noreply.github.com>
2018-11-27 17:05:04 +0100

    Update Item.java

--- Item.java
+++ Item.java
@@ -10,23 +10,40 @@ public class Item
 {
     protected String name;
     protected String description;
+    private static final String[] validItems = {
+      "Cookie", "Dentier", "Biberon", "Lait", "Clé", "Biberon de lait"
+    };
     /**
      * Constructor 
      * Create a new Item.
      * Each item's name must contain at least 1 character and doesn't have to start with a space.
-     * Description must contain at least 10 characters and doesn't have to start with a space.
+     * Description must contain at least 10 characters and have not to start with a space.
      * If one of those restrictions are not respected, the object cannot be created and an exception statement is returned.
      * 
      * @param name Proper name for the item created.
-     * @param description Proper description for the item created. 
+     * @param description Proper description for the item created.
      * @exception IllegalArgumentException Returned in case of name or description troubles. See restrictions above.
      */
     public Item(String name, String description)
     {
-        if (name.length()<1 || name.substring(0,1).equals(" ")) throw new IllegalArgumentException("Incorrect name");
+        if (isItem(name)==false) throw new IllegalArgumentException("Incorrect item's name");
         this.name = name;
         if (description.length()<10 || description.substring(0,1).equals(" ")) throw new IllegalArgumentException("Incorrect description");
         this.description = description;
+    }
+    /**
+     * This method check the presence of a given string in the list "validItems".
+     * 
+     * @return Return a boolean true if a given string is in the list of valid items. False if not.
+     */
+    private boolean isItem (String name)
+    {
+        for(int i = 0; i < validItems.length; i++) {
+            if(validItems[i].equals(name))
+                return true;
+        }
+        // if we get here, the string was not found in the commands
+        return false;
     }
     
     /**
