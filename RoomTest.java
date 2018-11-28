0fed982c0512577ef34bd3b233d170cd1bc1a979
Nairy86 <45037891+Nairy86@users.noreply.github.com>
2018-11-28 14:50:49 +0100

    Update RoomTest.java

--- RoomTest.java
+++ RoomTest.java
@@ -11,12 +11,13 @@ import java.util.*;
  * 
  * Les classes-test sont documentees ici :
  * http://junit.sourceforge.net/javadoc/junit/framework/TestCase.html
- * et sont basees sur le document Š 2002 Robert A. Ballance intitulee
+ * et sont basees sur le document Å  2002 Robert A. Ballance intitulee
  * "JUnit: Unit Testing Framework".
  */
 public class RoomTest
 {
     private Room roomTest;
+    private Item dentier;
     /**
      * Classe-test RoomTest constructor. 
      */
@@ -35,6 +36,7 @@ public class RoomTest
     public void setUp() 
     {
        roomTest = new Room ("azertyu est une salle random");
+       dentier = new Item("Denture","Buuuurk, there is mamy's slime on it");
     }
     
     /**
@@ -82,5 +84,26 @@ public class RoomTest
             result=false;
         }
         assertEquals(false,result);
+    }
+    
+    /**
+     * Method testAddItem() : Check if an item is correctly add to the inventory
+     */
+    @Test
+    public void testAddItemRoom()
+    {
+        roomTest.addInventoryRoom(dentier);
+        assertEquals(1,roomTest.inventoryRoom.size());
+    }
+    
+    /**
+     * Method testReturnInventory() : Check if the good string is returned when the character's inventory is non empty.
+     * Also check if nothing is returned in case of empty inventory.
+     */
+    @Test
+    public void testReturnInventoryRoom()
+    {
+        roomTest.addInventoryRoom(dentier);
+        assertEquals("1 Denture ;",roomTest.returnInventoryRoom());
     }
 }
