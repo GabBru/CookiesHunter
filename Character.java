92bcd940c00353ef9b33cd799fdc11d238da428b
Gabriel <gabriel_brunet@orange.fr>
2018-12-03 12:09:17 +0100

    Correction orthographe

--- Character.java
+++ Character.java
@@ -13,12 +13,12 @@ public class Character
      * Constructor
      * Create a new character.
      * Each character's name have to contain at least 3 characters. It also 
-     * cannot start with a space or contain more than 1 space ("firstname family_name").
+     * cannot start with a space or contain more than 1 space (ex : "firstname family_name").
      * If one of those restrictions are not respected, the object cannot be created and an Exception statement will be returned.
      * @param newName This parameter represent the name of the Character. It will be automatically initialised depending on which Character is created except for the Player class.
      * @param newRoom This parameter represent the current room where the character is located. It will evolve according to the progress of the game (Player case).
      * @param inventory Not given in the constructor method (always empty when characters are created).
-     * @exception IllegalArgumentException Returned in case of name's troubles - Less than 3 characters, more than one space or start with a space.
+     * @exception IllegalArgumentException Returned in case of name's troubles - Less than 3 characters, more than one space or started with a space.
      */
     public Character(String newName, Room newRoom)
     { 
