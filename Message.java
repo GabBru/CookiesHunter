58dbb12bbf79365370817ea95e56e9edfa59fc05
Nairy86 <45037891+Nairy86@users.noreply.github.com>
2018-12-05 17:15:04 +0100

    Create Message.java

---
+++ Message.java
@@ -0,0 +1,50 @@
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+
+/**
+ * This class is the class which contains the graphical representation
+ * of the messages and the entry field.
+ *
+ * @author Gp3 Amandine POULLOT
+ * @version 21/11/2018
+ */
+public class Message extends JPanel
+{
+    // déclarations variables
+    JPanel JPanelTxt, JPanelGet;
+    JLabel monLabel;
+    Player thisPlayer;
+    Message message;
+
+    public Message(Player nn)
+    { 
+        thisPlayer=nn;
+        //Creation of the objects (Buttons)
+        JPanelTxt = new JPanel();
+        JPanelGet = new JPanel();
+        setLayout(new BorderLayout());
+        JPanelTxt.setLayout(new GridLayout(1,1));
+        JPanelGet.setLayout(new FlowLayout());
+        //monLabel = new JLabel("ah que coucou", JLabel.CENTER);
+        monLabel = new JLabel(thisPlayer.getRoom(), JLabel.CENTER);
+        
+        JTextField textField = new JTextField();
+        textField.setColumns(10);
+        
+        // add in the panel
+       JPanelTxt.add(monLabel);
+       JPanelGet.add(textField);
+       
+        //Add object to the Frame
+        add(JPanelTxt, BorderLayout.NORTH);
+        add(JPanelGet, BorderLayout.SOUTH);
+       
+        
+        //Display elements
+        JPanelTxt.setVisible(true);
+        JPanelGet.setVisible(true);
+        setVisible(true); 
+    }
+}
+
