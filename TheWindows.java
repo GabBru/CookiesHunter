d6aed25b7e1f3e2ca991d2f6723478e708169789
Martin Pettinotti <martin.pettinotti@gmail.com>
2018-12-12 15:51:59 +0100

    Modif sur main window
    blabla

--- TheWindows.java
+++ TheWindows.java
@@ -1,38 +1,39 @@
-import java.awt.*;
+import java.util.*;
 import javax.swing.*;
+import java.awt.*;
 /**
- * Décrivez votre classe Image ici.
- * 
+ * Décrivez votre classe TheWindows ici.
  *
- * @author kilian felesmino
- * @version 05/12/2018
+ * @author (votre nom)
+ * @version (un numéro de version ou une date)
  */
-
 public class TheWindows extends JFrame
 {
     // variables d'instance - remplacez l'exemple qui suit par le vôtre
-    private ImageRoom test;
-    Player nn;
-    
+    private Game jeu;
     /**
-     * Constructeur d'objets de classe Image
+     * Constructeur d'objets de classe TheWindows
      */
-    public TheWindows (ImageRoom tata)
+    
+    public TheWindows(Game currentGame)
     {
-            test=tata;
-            nn = oo;
-            JFrame myFrame = new JFrame("plan");
-            myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            
-                       
-            myFrame.add(new Image(test));
-            myFrame.add(new Message(nn));
-            
-            
-            myFrame.pack();
-            myFrame.setVisible(true);
+        jeu = currentGame;
+        // initialisation des variables d'instance
+        JFrame myFrame = new JFrame("Test");
+        myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        myFrame.setSize(1600,900);
+        myFrame.setResizable(true);
+        myFrame.setLayout(new GridLayout(2,2));
+        myFrame.add(new Image(jeu.getImgCurrentRoom()));
+        myFrame.add(new JLabel(""));
+        myFrame.add(new ActionPanel(jeu.getPlayer()));
         
-
-        
+        //Display elements
+        myFrame.pack();
+        myFrame.setVisible(true);
+    }
+    
+    public void Initialisation () {
+        //Initialiser tout les ImageRoom        
     }
 }
