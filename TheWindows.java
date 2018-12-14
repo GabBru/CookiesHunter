92676b85bcfadc55e0bbf0fb299768fa1ef3e918
facina37 <45038209+facina37@users.noreply.github.com>
2018-12-14 17:26:38 +0100

    Update TheWindows.java

--- TheWindows.java
+++ TheWindows.java
@@ -22,6 +22,7 @@ public class TheWindows extends JFrame
 {
     private ImageRoom test;
     private Game myGame;
+    private JFrame myFrame;
     // creation of a pTop panel that contains the Image and Inventory panels
     //that will be organized in BorderLayout.
     private JPanel pTop, pBottom;
@@ -31,20 +32,20 @@ public class TheWindows extends JFrame
      */
     public TheWindows () //
     {
-                
         myGame = new Game();
-        JFrame myFrame = new JFrame("Hunter Cookies");
-        
+        myFrame = new JFrame("Hunter Cookies");
         myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         //myFrame.setSize(1600,900);
         myFrame.setResizable(true);
         myFrame.setLayout(new GridLayout(2,1));
-                
-        myFrame.add(new Image(myGame.getImgCurrentRoom()));
-        myFrame.add(new JLabel(""));
-        myFrame.add(new ActionPanel(this));
-        myFrame.add(new Info(this));
         
+        pTop = new Image(myGame.getImgCurrentRoom());
+        pBottom = new Info(this);
+        myFrame.add(new ActionPanel(this));
+        myFrame.add(new JLabel(""));
+        myFrame.add(pBottom);
+        myFrame.add(pTop);
+               
         myFrame.pack();
         myFrame.setVisible(true);
     }
@@ -52,4 +53,16 @@ public class TheWindows extends JFrame
     public Game getGame(){
         return myGame;
     }
+    
+    /*
+     * Method who refresh the Frame
+     */
+    public void refresh(){
+        pBottom.removeAll();
+        pTop.removeAll();
+        pBottom.add(new Info(this));
+        pTop.add(new Image(myGame.getImgCurrentRoom()));        
+        myFrame.revalidate();
+        myFrame.repaint();
+    }  
 }
