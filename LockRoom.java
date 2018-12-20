48394cdb92c04026189b8f1779272f55b24a2ed4
facina37 <45038209+facina37@users.noreply.github.com>
2018-12-20 17:45:16 +0100

    Update LockRoom.java

--- LockRoom.java
+++ LockRoom.java
@@ -1,10 +1,9 @@
-// import java.util.*
 /**
  * The class LockRoom inherites the super class Room.
  * This class links all rooms that are blocked by a key or a password.
  * 
  * @author Antoine PIERSON & Marie Bogusz
- * @version v0.2 2018nov
+ * @version 05/12/2018
  */
 public class LockRoom extends Room
 {
@@ -14,20 +13,20 @@ public class LockRoom extends Room
      * 
      * This class inherite the same criteria of the class Room for the description. 
      * The description should not be short.One letter does not constitue a description. 
-     * If we try to do that, an exception will be returned. 
-     * And the room cannot be created.
+     * If we try to do that, an exception will be returned and the room cannot be created.
      * 
      * @param description See details in superclass. 
-     * @param isLocked This attributes allows us to manage the accessibility of a room to an other.We cannot access it without unlocking it with a key (Item).
+     * @param isLocked This attributes allows us to manage the accessibility of a room to an other.
+     * We cannot access it without unlocking it with a key Item.
      */
     public LockRoom(String nameRoom, String description)
     {
-        super(nameRoom, description);
-        isLocked = true;
+        super(nameRoom, description); //These two variables are inherited from the parent class Room.
+        isLocked = true; //A new LockRoom is closed.
     }
 
     /**
-     * This method inform us on the blocked or unblocked status of the room. 
+     * This method inform us on the locked or unlocked status of the room. 
      * @return isLocked Return the boolean isLocked.
      */
     public boolean getLocked()
