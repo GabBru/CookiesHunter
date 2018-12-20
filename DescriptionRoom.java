9b9bda42f7ed5cb54dcd1f90b69a9712372027be
facina37 <45038209+facina37@users.noreply.github.com>
2018-12-20 17:13:12 +0100

    Update DescriptionRoom.java

--- DescriptionRoom.java
+++ DescriptionRoom.java
@@ -1,41 +1,38 @@
 import java.awt.*;
-import java.awt.event.*;
-
 import javax.swing.*;
-import javax.swing.event.*;
-import javax.swing.text.*;
-
-import java.beans.PropertyChangeListener;
-import java.beans.PropertyChangeEvent;
-
 import java.text.*;
 
 /**
- * this class will allow a display of information such as the name of the player, the current room ...
+ * This class allow to display the desciption of the current room.
+ * The description attribute can contain information about the current room, the characters dialog ...
  *
  * @author Marie Bogusz
- * @version 15/12/18
+ * @version 20/12/18
  */
 public class DescriptionRoom extends JPanel
 {
+    // ----- ATTRIBUTS OF THE INTERFACE -----
+    //Variable of labels
     private JPanel myPanel;
     private JLabel myLabel1;
-    private Game myGame;
-     
     
+    // ----- ATTRIBUT GAME -----
+    private Game myGame;  
+    /**
+     * DescriptionRoom() is the constructor of class DescriptionRoom
+     * It allows you to create a Panel that contains text. 
+     * This text describes the current room and other things.
+     * This constructor will be called in our TheWindows Frame.
+     */
     public DescriptionRoom(TheWindows win){
-        myGame = win.getGame();
-        
-        setLayout(new GridLayout());
-
+            //Game recovery
+        myGame = win.getGame(); 
+            //Adding the description of current room in the Label
         myLabel1 = new JLabel(myGame.getPlayer().getRoom().getDescription());
-
-        //Add the Label in myPanel
+            //Add the Label in myPanel
         JPanel labelPane = new JPanel(new GridLayout(0,1));
         labelPane.add(myLabel1);
-        
         setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
-        add(labelPane, BorderLayout.CENTER);
-        
+        add(labelPane, BorderLayout.CENTER);        
     }
 }
