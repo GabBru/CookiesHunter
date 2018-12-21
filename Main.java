9bde7492abe1e17c09a6415d1f3a9e0bb42c09a2
Gabriel Brunet <33067666+GabBru@users.noreply.github.com>
2018-12-21 11:51:56 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,16 +1,14 @@
-
 /**
- * Decrivez votre classe Main ici.
+ * This class is the main class of the program which allows to launch the game directly.
  *
- * @author Groupe 3
- * @version v0.1 2018dec
+ * @author Groupe3 : Martin Pettinotti, Amandine Poullot, Gabriel Brunet, Antoine Pierson, Marie Bogusz, Thibaud Gil, Diama Fall, Kilian Felesmino.
+ * @version v0.2 2018dec
  */
 public class Main
 {
     public static void main()
     {
         try{
-            //Game myGame = new Game("azertyui");
             TheWindows myWindows = new TheWindows();
         } catch (Exception e){
             e.printStackTrace();
