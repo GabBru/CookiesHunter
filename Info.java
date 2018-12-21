c9a53f79ff287545ec14cc5d4884938e6d040265
Gabriel Brunet <33067666+GabBru@users.noreply.github.com>
2018-12-21 11:25:30 +0100

    Update Info.java

--- Info.java
+++ Info.java
@@ -1,40 +1,29 @@
 import java.awt.*;
-import java.awt.event.*;
-
 import javax.swing.*;
-import javax.swing.event.*;
-import javax.swing.text.*;
-
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeEvent;
-
 import java.text.*;
-
 /**
- * this class will allow a display of information such as the name of the player, the current room ...
+ * This class will allow to display information such as the name of the player, the level of the player and the current room where the player is located.
  *
  * @author Diama FALL
- * @version 11/12/18
+ * @version v0.2 2018dec
  */
 public class Info extends JPanel
 {
-    JPanel myPanel;
-    JLabel myLabel1, myLabel2, myLabel3;
+    private JPanel myPanel;
+    private JLabel myLabel1, myLabel2, myLabel3;
     private Game myGame;
-    Player dCharacter; 
-        
+    private Player dCharacter; 
+    
     public Info(TheWindows win){
         myGame = win.getGame();
-        
-        // set the different Labels
+        // create Label PlayerName
         myLabel1 = new JLabel("Name : "+ myGame.getPlayer().getName());
-
-        //createLabel Level
+        // createLabel Level
         myLabel2 = new JLabel("Level : "+myGame.getPlayer().getLevel());
-
-        //create Label CurrentRoom
+        // create Label CurrentRoom
         myLabel3 = new JLabel("Current room : " + myGame.getPlayer().getRoom().getNameRoom());
-
         //Add the Label in myPanel
         JPanel labelPane = new JPanel(new GridLayout(0,1));
         labelPane.add(myLabel1);
